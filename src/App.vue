1f5c493ba1d874b740ccad4f0e5c474f42a9b8dd
thamaragerigr <thamaragerigr@gmail.com>
2020-05-27 23:07:52 +0200

    added title style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    Notes
+    <div>
+      <div class="dots">
+        <span class="yellow"></span>
+        <span class="red"></span>
+        <span class="green"></span>
+      </div>
+      <h1>Notes</h1>
+    </div>
     <NotesList/>
   </div>
 </template>
@@ -17,12 +24,47 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  background: #22343C;
+  color: white;
+  font-family: 'Quicksand', sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+#app{
+  margin: auto;
+  max-width: 375px;
+  min-height: 665px;
+  padding: 10px;
+  border: 1px red solid;
+}
+
+.dots{
+  display: flex;
+  justify-content: center;
+}
+.yellow{
+  margin: 5px;
+  width: 15px;
+  height: 10px;
+  background: #FFC542;
+  border-radius: 6px;
+}
+.red{
+  margin: 5px;
+  width: 15px;
+  height: 10px;
+  background: #FF575F;
+  border-radius: 6px;
+}
+.green{
+  margin: 5px;
+  width: 15px;
+  height: 10px;
+  background: #3DD598;
+  border-radius: 6px;
+}
+h1{
+  margin: 0;
 }
 </style>
